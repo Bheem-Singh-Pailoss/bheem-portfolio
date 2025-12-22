aa7f5a6a1754aaccb0208cccd7a60e2d59ce6085
Bheem-Singh-Pailoss <mnegi0876@gmail.com>
2025-12-22 19:45:57 +0530

    Updates

--- _next/static/cEakYAQwSwAbp3lRWRA7F/_buildManifest.js
+++ _next/static/cEakYAQwSwAbp3lRWRA7F/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/bheem-portfolio//_next/:path+",
-        "destination": "/bheem-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
